2f5987967a9f9ed528d30a11e37284925cdf9b20
Anushka Indoria <indoriaanushka@gmail.com>
2026-06-12 12:57:28 +0530

    Connect backend to Neon

--- backend/db.ts
+++ backend/db.ts
@@ -3,7 +3,7 @@ import pg from "pg";
 
 const { Pool } = pg;
 
-//console.log("DATABASE_URL =", process.env.DATABASE_URL);
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
 
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
